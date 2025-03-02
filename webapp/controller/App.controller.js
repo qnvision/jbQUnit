5d1c1a86a82c78989f00f4da32ea7c9bb2c9e1f6
Hadas kiriati <hadas.kiriati@qnvision.com>
2025-03-02 15:00:48 +0000

    .

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller"
+], (BaseController) => {
+  "use strict";
+
+  return BaseController.extend("jbqunittest.controller.App", {
+      onInit() {
+      }
+  });
+});
