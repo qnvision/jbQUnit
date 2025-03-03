e755e9e45bd87c06ef3bc6ae5f6f0f3274ae2721
Hadas kiriati <hadas.kiriati@qnvision.com>
2025-03-03 11:37:34 +0000

    first commit

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -5,6 +5,27 @@ sap.ui.define([
 
     return Controller.extend("jbqunittest.controller.View1", {
         onInit() {
+        },
+        arraySort: function(arr, bDescending) {
+            function fSort(a, b) {
+                if (a > b) {
+                    return bDescending ? -1 : 1;
+                } else if (a < b) {
+                    return bDescending ? 1 : -1;
+                } else {
+                    return 0;
+                }
+            };
+            return arr.sort(fSort);
+        },
+        countElement: function(arr, searchElement) {
+            var i = 0;
+            arr.forEach(element => {
+                if (element === searchElement) {
+                    i++;
+                }
+            });
+            return i;
         }
     });
 });
--- webapp/test/unit/controller/View1.controller.js
+++ webapp/test/unit/controller/View1.controller.js
@@ -5,12 +5,39 @@ sap.ui.define([
 ], function (Controller) {
 	"use strict";
 
-	QUnit.module("View1 Controller");
+    QUnit.module("View1 Controller", {
+        beforeEach: function () {
+            this.oController = new Controller();
+        },
+        afterEach: function () {
+            this.oController.destroy();
+        }
+    });
 
-	QUnit.test("I should test the View1 controller", function (assert) {
-		var oAppController = new Controller();
-		oAppController.onInit();
-		assert.ok(oAppController);
-	});
+    QUnit.test("Should sort array in ascending order", function (assert) {
+        let aInput = [3, 1, 2];
+        let aExpected = [1, 2, 3];
+        let aResult = this.oController.arraySort(aInput, false);
+        assert.deepEqual(aResult, aExpected, "The array is sorted correctly in ascending order");
+    });
+
+    QUnit.test("Should sort array in descending order", function (assert) {
+        let aInput = [3, 1, 2];
+        let aExpected = [3, 2, 1];
+        let aResult = this.oController.arraySort(aInput, true);
+        assert.deepEqual(aResult, aExpected, "The array is sorted correctly in descending order");
+    });
+
+    QUnit.test("Should count occurrences of an element", function (assert) {
+        let aInput = [1, 2, 3, 2, 2, 4];
+        let iResult = this.oController.countElement(aInput, 2);
+        assert.strictEqual(iResult, 3, "The count of element 2 is correct");
+    });
+
+    QUnit.test("Should return zero for non-existing element", function (assert) {
+        let aInput = [1, 2, 3, 4, 5];
+        let iResult = this.oController.countElement(aInput, 10);
+        assert.strictEqual(iResult, 0, "The count of element 10 is correctly returned as 0");
+    });
 
 });
